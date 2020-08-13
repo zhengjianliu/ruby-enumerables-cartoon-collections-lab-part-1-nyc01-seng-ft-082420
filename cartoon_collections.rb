b013721529ac48e24a3d707bf089c1f95ce318ec
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-13 02:09:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,6 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  hash = Hash.new
   array.each_with_index |everyone,index|
   puts "#{index}. #{everyone}!"
 end
