6b9ea1d37e33c2d5ad38b09ba30e2f9908c5ad71
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-13 02:02:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,6 +5,7 @@ def greet_characters(array)
   array.each do |everyone|
   puts "Hello #{everyone}!"
 end
+end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
