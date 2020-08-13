127c0635b4d84b642185fde6612596ad36519b18
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-13 02:12:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   %w(array).each_with_index { |item, index|
-  array[index] = "#{index}. #{item}"
+  puts "#{index}. #{item}"
   }
 end
 end
